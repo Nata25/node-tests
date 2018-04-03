54442693cd0839fab74b6eb645dc13d873de2957
Natalya Ivanova <natalyas.ivanova@gmail.com>
2018-04-03 17:36:41 +0300

    Use describe() to reorganize tests

--- server/server.test.js
+++ server/server.test.js
@@ -3,34 +3,37 @@ const expect = require('expect');
 
 const app = require('./server').app;
 
-it('should return \'hello world\'', (done) => {
-  request(app)
-  .get('/')
-  .expect(200)
-  .expect('Hello, world!')
-  .end(done);
-});
+describe('Server', () => {
+  it('should return \'hello world\'', (done) => {
+    request(app)
+    .get('/')
+    .expect(200)
+    .expect('Hello, world!')
+    .end(done);
+  });
+  
+  it('should return error message', (done) => {
+    request(app)
+    .get('/bad')
+    .expect(404)
+    .expect((res) => {
+      expect(res.body).toInclude({
+        error: 'Page not found.'
+      });
+    })
+    .end(done);
+  });
+  
+  it('should find a user in users array', (done) => {
+    request(app)
+    .get('/users')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.users).toInclude(
+        { name: 'Natalia', age: 35 }
+      );
+    })
+    .end(done);
+  });
 
-it('should return error message', (done) => {
-  request(app)
-  .get('/bad')
-  .expect(404)
-  .expect((res) => {
-    expect(res.body).toInclude({
-      error: 'Page not found.'
-    });
-  })
-  .end(done);
 });
-
-it('should find a user in users array', (done) => {
-  request(app)
-  .get('/users')
-  .expect(200)
-  .expect((res) => {
-    expect(res.body.users).toInclude(
-      { name: 'Natalia', age: 35 }
-    );
-  })
-  .end(done);
-})
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,56 +1,68 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  const result = utils.add(33, 11);
-  expect(result).toBeA('number');
-  expect(result).toBe(44);
-});
+describe('Utils', () => {
 
-it('should square a number', () => {
-  const result = utils.square(4);
-  // if (result !== 16) {
-    // throw new Error(`Expected 9 but got ${result}`);
-  // }
-  expect(result).toBeA('number');
-  expect(result).toBe(16);
-});
-
-it('should be included in object', () => {
-  expect({
-    name: 'Natalya',
-    age: 35
-  }).toInclude({
-    age: 35
+  describe('#Add', () => {
+    it('should add two numbers', () => {
+      const result = utils.add(33, 11);
+      expect(result).toBeA('number');
+      expect(result).toBe(44);
+    });
+    
+    it('should add two numbers asyncroniously', (done) => {
+      utils.asyncAdd(3, 4, (sum) => {
+        expect(sum).toBeA('number').toBe(7);
+        done();
+      });
+    });
   });
-});
 
-it('should verify first and last names are set', () => {
-  const user = {
-    age: 30,
-    location: 'Ukraine',
-  };
+  describe('#Square', () => {
+    it('should square a number', () => {
+      const result = utils.square(4);
+      // if (result !== 16) {
+        // throw new Error(`Expected 9 but got ${result}`);
+      // }
+      expect(result).toBeA('number');
+      expect(result).toBe(16);
+    });
 
-  const updatedUser = utils.setName(user, 'Natalia Ivanova');
+    it('should square two numbers asyncroniously', (done) => {
+      utils.asyncSquare(4, (sum) => {
+        expect(sum).toBeA('number').toBe(16);
+        done();
+      });
+    });
 
-  expect(updatedUser).toBeA('object');
-
-  expect(updatedUser).toInclude({
-    firstName: 'Natalia',
-    lastName: 'Ivanova'
   });
+  
+  describe('#Objects', () => {
+    it('should be included in object', () => {
+      expect({
+        name: 'Natalya',
+        age: 35
+      }).toInclude({
+        age: 35
+      });
+    });
+    
+    it('should verify first and last names are set', () => {
+      const user = {
+        age: 30,
+        location: 'Ukraine',
+      };
+    
+      const updatedUser = utils.setName(user, 'Natalia Ivanova');
+    
+      expect(updatedUser).toBeA('object');
+    
+      expect(updatedUser).toInclude({
+        firstName: 'Natalia',
+        lastName: 'Ivanova'
+      });
+    });
+  });
+
 });
 
-it('should add two numbers asyncroniously', (done) => {
-  utils.asyncAdd(3, 4, (sum) => {
-    expect(sum).toBeA('number').toBe(7);
-    done();
-  });
-});
-
-it('should square two numbers asyncroniously', (done) => {
-  utils.asyncSquare(4, (sum) => {
-    expect(sum).toBeA('number').toBe(16);
-    done();
-  });
-});
