a1fcdd873a333e13a883f25142ae665f8e738f87
Natalya Ivanova <natalyas.ivanova@gmail.com>
2018-04-03 17:29:15 +0300

    Explore response by using expect()

--- server/server.js
+++ server/server.js
@@ -8,9 +8,25 @@ app.get('/', (req, res) => {
 
 app.get('/bad', (req, res) => {
   res.status(404).send({
-    error: 'Page not found'
+    error: 'Page not found.',
+    name: 'Express App v.1.0'
   });
 });
+
+app.get('/users', (req, res) => {
+  res.status(200).send({
+    users: [
+      {
+        name: 'Andrew',
+        age: 25,
+      },
+      {
+        name: 'Natalia',
+        age: 35
+      }
+    ]
+  })
+})
 
 app.listen('3000');
 
--- server/server.test.js
+++ server/server.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const expect = require('expect');
 
 const app = require('./server').app;
 
@@ -14,8 +15,22 @@ it('should return error message', (done) => {
   request(app)
   .get('/bad')
   .expect(404)
-  .expect({
-    error: 'Page not found'
+  .expect((res) => {
+    expect(res.body).toInclude({
+      error: 'Page not found.'
+    });
+  })
+  .end(done);
+});
+
+it('should find a user in users array', (done) => {
+  request(app)
+  .get('/users')
+  .expect(200)
+  .expect((res) => {
+    expect(res.body.users).toInclude(
+      { name: 'Natalia', age: 35 }
+    );
   })
   .end(done);
 })
